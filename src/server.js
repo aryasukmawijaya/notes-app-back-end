bc52e3028753d5eae6c4899b64d8e0751f6eacd4
Arya Sukma Wijaya <aryasukma.work@gmail.com>
2021-06-06 07:52:32 +0700

    Set env and prod

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
-const routes = require('./src/routes.js');
+const routes = require('./routes.js');
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*']
